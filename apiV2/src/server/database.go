94542c4ba7eee9f10901e711ac8f5ee9dd635787
Mac Deluca <Mac.Deluca@quartech.com>
2024-07-31 15:39:22 -0700

    feat: added dev setup for go

--- apiV2/src/server/database.go
+++ apiV2/src/server/database.go
@@ -7,32 +7,31 @@ import (
 )
 
 var (
-	host     = os.Getenv("API_HOST")
-	port     = os.Getenv("API_PORT")
+	host     = os.Getenv("DB_HOST")
+	port     = os.Getenv("DB_PORT")
 	user     = os.Getenv("DB_USER_API")
 	password = os.Getenv("DB_USER_API_PASSWORD")
 	database = os.Getenv("DB_DATABASE")
 )
 
-// Create a new database connection
-func NewDBConnection() *sql.DB {
+func SetupDatabase() *sql.DB {
 	psqlInfo := fmt.Sprintf(`
     host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
 		host, port, user, password, database)
 
-	connection, err := sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", psqlInfo)
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer connection.Close()
+	defer db.Close()
 
-	err = connection.Ping()
+	err = db.Ping()
 
 	if err != nil {
 		panic(err)
 	}
 
-	return connection
+	return db
 }
--- apiV2/src/server/server.go
+++ apiV2/src/server/server.go
@@ -9,37 +9,35 @@ import (
 	"recipehub/api/src/handler"
 )
 
-// Environment variables
 var PORT = ":" + os.Getenv("API_PORT")
 
 // Add routes and inject dependencies to handlers
-func addRoutes(router *http.ServeMux, connection *sql.DB) {
+func addRoutes(router *http.ServeMux, db *sql.DB) {
 	router.HandleFunc("GET /api", handler.Root)
 }
 
 // Creates a new router, adds routes and applies middleware
-func newRouter() *http.ServeMux {
+func newRouter(db *sql.DB) *http.ServeMux {
 	router := http.NewServeMux()
 
-	connection := NewDBConnection()
+	addRoutes(router, db)
 
-	addRoutes(router, connection)
-
-	//handler = someMiddleware(handler)
+	//router = someMiddleware(handler)
 
 	return router
 }
 
 // Start http server
 func Start() {
-	router := newRouter()
+	db := SetupDatabase()
+	router := newRouter(db)
 
 	server := http.Server{
 		Addr:    PORT,
 		Handler: router,
 	}
 
-	log.Println("Starting server on port: %s", PORT)
+	log.Println("Starting server on port", PORT)
 
 	err := server.ListenAndServe()
 
@@ -47,5 +45,4 @@ func Start() {
 		fmt.Printf("Error starting server: %s\n", err)
 		os.Exit(1)
 	}
-
 }
