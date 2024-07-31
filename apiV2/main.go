eb3c7dcaf87d1cda5e524bf7740ef49c2a62a58c
Mac Deluca <Mac.Deluca@quartech.com>
2024-07-30 20:29:44 -0700

    feat: moved files around in go api

---
+++ apiV2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"recipehub/api/controllers/get"
+
+	_ "github.com/lib/pq"
+)
+
+var PORT = os.Getenv("API_PORT")
+
+func main() {
+	http.HandleFunc("/api", get.Root)
+	http.HandleFunc("/api/cookbooks", get.UserCookbooks)
+	// http.HandleFunc("/api/cookbooks/{id}", get.UserCookbooks)
+	// http.HandleFunc("/api/cookbooks/{id}/recipes", get.UserCookbooks)
+	// http.HandleFunc("/api/cookbooks/{id}/recipes/{id}", get.UserCookbooks)
+	// http.HandleFunc("/api/cookbooks/{id}/recipes/{id}/ingredients", get.UserCookbooks)
+	// http.HandleFunc("/api/cookbooks/{id}/recipes/{id}/ingredients{id}", get.UserCookbooks)
+
+	err := http.ListenAndServe(PORT, nil)
+
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
