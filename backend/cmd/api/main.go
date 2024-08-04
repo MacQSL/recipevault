3e05fdc39bacf70ebc7feea66b5e496dce7070e4
Mac Deluca <Mac.Deluca@quartech.com>
2024-08-03 23:20:32 -0700

    feat: updated github workflows

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"net/http"
 	"recipevault/api"
-	"recipevault/db"
-	"recipevault/util"
 
 	_ "github.com/lib/pq"
 )
 
-// Run http server
 func main() {
-	log := util.NewLogger()
-	conf := util.NewConfig()
-
-	mux := http.NewServeMux()
-	db := db.ConnectDB(log, conf)
-
-	router := api.NewRouter(mux, log, db)
-
-	server := http.Server{
-		Addr:    ":" + conf.API_PORT,
-		Handler: router,
-	}
-
-	log.Info("Starting server on port", conf.API_PORT)
-
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal("Error starting server:", err)
-	}
+	api.StartServer()
 }
